84928745ee2fa10d237dd3f5762009065db846a5
Vitor Ferreira Bem Silva <vfr@certi.org.br>
2021-01-22 00:00:51 -0300

    adds custom errors to the services

--- services/giphyService.js
+++ services/giphyService.js
@@ -1,12 +1,18 @@
 const fetch = require('node-fetch');
 
+const { GiphyApiFetchError, GiphyKeyInvalidError } = require('../error/giphyServiceError.js');
+
 const getRecipeGiphyURL = async (text) => {
+  if (!process.env.GIPHY_API_KEY) {
+    throw new GiphyKeyInvalidError();
+  }
+
   try {
     const response = await fetch(`${process.env.GIPHY_HOST}/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${text}&limit=1&rating=${process.env.GIPHY_CONTENT_RATING}`);
     const results = await response.json();
-    return results.data[0].url;
+    return results;
   } catch (e) {
-    return new Error('error');
+    throw new GiphyApiFetchError();
   }
 };
 
--- services/recipePuppyService.js
+++ services/recipePuppyService.js
@@ -1,12 +1,14 @@
 const fetch = require('node-fetch');
 
+const { RecipePuppyApiError } = require('../error/recipePuppyError.js');
+
 const getRecipePuppyRecipes = async (ingredients) => {
   try {
     const response = await fetch(`${process.env.RECIPE_PUPPY_HOST}/?i=${ingredients}`);
     const recipes = await response.json();
     return recipes.results;
   } catch (e) {
-    throw new Error();
+    throw new RecipePuppyApiError();
   }
 };
 

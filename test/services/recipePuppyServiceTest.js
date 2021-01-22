9942d3e80fa593d29e0b989ba4e6ce82e0873434
Vitor Ferreira Bem Silva <vfr@certi.org.br>
2021-01-22 15:14:25 -0300

    fixes recipePuppyService tests paramaters

--- test/services/recipePuppyServiceTest.js
+++ test/services/recipePuppyServiceTest.js
@@ -4,24 +4,22 @@ const getRecipePuppyRecipesTest = () => {
   describe('getRecipePuppyRecipes()', () => {
     it('it should return the Giphy API data', async () => {
       const recipes = await getRecipePuppyRecipes('cheese');
-      recipes.should.be.a('array');
+      recipes.should.be.a('Array');
       recipes[0].should.be.a('object');
       recipes[0].should.have.property('title');
-      recipes[0].should.have.property('href');
       recipes[0].should.have.property('ingredients');
-      recipes[0].should.have.property('thumbnail');
+      recipes[0].should.have.property('href');
     });
   });
 
   describe('getRecipePuppyRecipes()', () => {
     it('should return an open selection of recipes since there were no ingredients provided', async () => {
       const recipes = await getRecipePuppyRecipes('');
-      recipes.should.be.a('array');
-      recipes[0].should.be.a('object');
+      recipes.should.be.a('Array');
+      recipes[0].should.be.a('Object');
       recipes[0].should.have.property('title');
-      recipes[0].should.have.property('href');
       recipes[0].should.have.property('ingredients');
-      recipes[0].should.have.property('thumbnail');
+      recipes[0].should.have.property('href');
     });
   });
 };

f69f2e7b817df412aff14a90dcbb5c8bf8b8492d
Vitor Ferreira Bem Silva <vfr@certi.org.br>
2021-01-22 14:55:49 -0300

    adds the recipePuppyService, index and invalid route tests to test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,16 @@
 const { validRequestsRecipeControllerTest } = require('./controllers/validRequestsRecipeControllerTest.js');
 const { invalidRequestsRecipeControllerTest } = require('./controllers/invalidRequestsRecipeControllerTest.js');
 const { getRecipeGiphyURLTest } = require('./services/giphyServiceTest.js');
+const { getRecipePuppyRecipesTest } = require('./services/recipePuppyServiceTest.js');
+const { indexTest } = require('./indexTest.js');
+const { invalidRouteTest } = require('./invalidRouteTest.js');
 
 validRequestsRecipeControllerTest();
 invalidRequestsRecipeControllerTest();
+
 getRecipeGiphyURLTest();
+getRecipePuppyRecipesTest();
+
+indexTest();
+
+invalidRouteTest();

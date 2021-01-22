1595580cb8de3369ace2192694e4d68967aa6acb
Vitor Ferreira Bem Silva <vfr@certi.org.br>
2021-01-22 00:00:33 -0300

    fixes used assertions and removes invalid assertions

--- test/test.js
+++ test/test.js
@@ -1,95 +1,89 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-let should = chai.should();
-let host = "http://localhost:4567";
+require('dotenv').config();
+
+const should = chai.should();
+const host = 'http://localhost:4567';
 
 chai.use(chaiHttp);
 
 function recipeTests(host) {
-
-  describe('/GET /recipes/i=onion', () => {
+  describe('GET /recipes/?i=cheese', () => {
     it('should return recipes', (done) => {
       chai.request(host)
-          .get('/recipes/i=onion')
-          .end((error, response) => {
-                response.should.have.status(200);
-                response.body.title.should.be.a('String');
-                response.body.keywords.should.be.a('Array');
-                response.body.keywords.should.be.eql(["onion"]);
-                response.body.recipes.should.be.a('Array');
-            done();
-          });
+        .get('/recipes/?i=cheese')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.body.message.keywords.should.be.a('Array');
+          response.body.message.keywords.should.be.eql(['cheese']);
+          response.body.message.recipes.should.be.a('Array');
+          done();
+        });
     });
   });
 
-  describe('/GET /recipes/i=onion,tomato', () => {
+  describe('GET /recipes/?i=cheese,tomato', () => {
     it('should return recipes', (done) => {
       chai.request(host)
-          .get('/recipes/i=onion,tomato')
-          .end((error, response) => {
-                response.should.have.status(200);
-                response.body.title.should.be.a('String');
-                response.body.keywords.should.be.a('Array');
-                response.body.keywords.should.be.eql(["onion","tomato"]);
-                response.body.recipes.should.be.a('Array');
-            done();
-          });
+        .get('/recipes/?i=cheese,tomato')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.body.message.keywords.should.be.a('Array');
+          response.body.message.keywords.should.be.eql(['cheese', 'tomato']);
+          response.body.message.recipes.should.be.a('Array');
+          done();
+        });
     });
   });
 
-  describe('/GET /recipes/i=onion,tomato,chicken', () => {
+  describe('GET /recipes/?i=cheese,tomato,chicken', () => {
     it('should return recipes', (done) => {
       chai.request(host)
-          .get('/recipes/i=onion,tomato')
-          .end((error, response) => {
-                response.should.have.status(200);
-                response.body.title.should.be.a('String');
-                response.body.keywords.should.be.a('Array');
-                response.body.keywords.should.be.eql(["onion","tomato","chicken"]);
-                response.body.recipes.should.be.a('Array');
-            done();
-          });
+        .get('/recipes/?i=cheese,tomato,chicken')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.body.message.keywords.should.be.a('Array');
+          response.body.message.keywords.should.be.eql(['cheese', 'tomato', 'chicken']);
+          response.body.message.recipes.should.be.a('Array');
+          done();
+        });
     });
   });
 
-  describe('/GET /recipes/i=onion,tomato,chicken,pasta', () => {
+  describe('GET /recipes/?i=cheese,tomato,chicken,pasta', () => {
     it('should return an error message for having too many ingredients', (done) => {
       chai.request(host)
-          .get('/recipes/i=onion,tomato')
-          .end((error, response) => {
-                response.should.have.status(400);
-                response.body.message.should.be.eql("Too many ingredients provided. please provide only a maximum of 3 ingredients.");
-            done();
-          });
+        .get('/recipes/?i=cheese,tomato,pato,onions')
+        .end((error, response) => {
+          response.should.have.status(400);
+          response.body.message.should.be.eql('You have provided too many ingredients. Please provide a maximum of 3 ingredients, and a minimum of 1.');
+          done();
+        });
     });
   });
 
-  describe('/GET', () => {
+  describe('GET', () => {
     it('should return an error message for having no ingredients', (done) => {
       chai.request(host)
-          .get('/recipes/')
-          .end((error, response) => {
-                response.should.have.status(400);
-                response.body.message.should.be.eql("No ingredients provided. please at least 1 ingredient.");
-            done();
-          });
+        .get('/recipes/')
+        .end((error, response) => {
+          response.should.have.status(400);
+          response.body.message.should.be.eql('i is a required field');
+          done();
+        });
     });
   });
 
-  describe('/GET', () => {
+  describe('GET', () => {
     it('should return an empty recipe list', (done) => {
       chai.request(host)
-          .get('/recipes/i=nonexistent')
-          .end((error, response) => {
-                response.should.have.status(200);
-                response.body.title.should.be.a('String');
-                response.body.keywords.should.be.a('Array');
-                response.body.keywords.should.be.eql(["onion","tomato","chicken"]);
-                recipes_array_length = response.body.recipes.length;
-                recipes_array_length.should.be.eql(0);
-            done();
-          });
+        .get('/recipes/?i=nonexistent')
+        .end((error, response) => {
+          response.should.have.status(400);
+          response.body.message.should.be.eql('The searched ingredients have retrieved no results. Please try with other ingredients.');
+          done();
+        });
     });
   });
 }
@@ -97,5 +91,5 @@ function recipeTests(host) {
 recipeTests(host);
 
 module.exports = {
-  recipeTests
+  recipeTests,
 };

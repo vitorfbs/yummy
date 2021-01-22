304a9940545d0d80d8e17d37947dc963b3b62754
Vitor Ferreira Bem Silva <vfr@certi.org.br>
2021-01-22 15:31:33 -0300

    renames error folders

--- errors/error.js
+++ errors/error.js
@@ -1,8 +1,8 @@
-const { GiphyApiFetchError } = require('./giphyService/giphyApiFetchError.js');
-const { GiphyKeyInvalidError } = require('./giphyService/giphyKeyInvalidError.js');
-const { TooManyIngredientsProvidedError } = require('./recipeController/tooManyIngredientsProvidedError.js');
-const { RecipePuppyApiError } = require('./recipePuppyService/recipePuppyApiError.js');
-const { RecipePuppyEmptyRecipeListError } = require('./recipePuppyService/recipePuppyEmptyRecipeListError.js');
+const { GiphyApiFetchError } = require('./giphyServiceError/giphyApiFetchError.js');
+const { GiphyKeyInvalidError } = require('./giphyServiceError/giphyKeyInvalidError.js');
+const { TooManyIngredientsProvidedError } = require('./recipeControllerError/tooManyIngredientsProvidedError.js');
+const { RecipePuppyApiError } = require('./recipePuppyServiceError/recipePuppyApiError.js');
+const { RecipePuppyEmptyRecipeListError } = require('./recipePuppyServiceError/recipePuppyEmptyRecipeListError.js');
 
 module.exports = {
   GiphyApiFetchError,
